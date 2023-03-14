dfb4b6040a87f91a259ab1ff528a926b733cc1f1
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-13 20:11:04 -0700

    complette

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    evens_list = [n for n in num_list if n % 2 == 0]
+    return evens_list
 def make_exclamation(sentence_list):
-    pass
+    new_sentences = [n + "!" for n in sentence_list]
+    return new_sentences
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
